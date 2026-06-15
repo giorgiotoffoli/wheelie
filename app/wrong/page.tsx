d9549ca7bcce6000a5fa83e8d69baa9d6e449e70
Gio <giorgio.toffoli@proton.me>
2026-06-15 11:03:23 -0500

    Remove useParams in /wrong

--- app/wrong/page.tsx
+++ app/wrong/page.tsx
@@ -1,9 +1,26 @@
-import { Suspense } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function WrongAreaPage() {
+  const router = useRouter()
   return (
-    <Suspense
-      fallback={<p className="text-center mt-24">Loading...</p>}
-    ></Suspense>
+    <div>
+      <div className="h-full text-center pt-24 flex align-middle flex-col">
+        <h1 className="font-bold text-3xl">Oops, wrong area!</h1>
+
+        <p className="opacity-75 text-lg mx-4">
+          Please check the wheelchair area to ensure it is correct.{' '}
+          <a href="#" className="underline">
+            Need help?
+          </a>
+        </p>
+
+        <button
+          className="mt-12 p-2 border-2 border-white rounded-2xl bg-white text-black hover:bg-transparent transition duration-0.5s hover:text-white hover:cursor-pointer mx-12"
+          onClick={() => router.push('/wheelchair')}
+        >
+          <p>Scan another</p>
+        </button>
+      </div>
+    </div>
   )
 }
